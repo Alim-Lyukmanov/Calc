ba1c4966e52c80d528ee5a02fb7bf7698c35dcf1
Alim Lyukmanov <arlyukmanov@mail.ru>
2022-11-29 03:40:59 +0300

    Класс для инициализации параметров UI элемента

---
+++ Calculator/UiElementInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Navigation;
+
+internal class UiElementInfo : IUiElement
+{
+    public string Name { get { return name; }  set => name = value; }
+    public string Content { get { return content; } set => content = value; }
+    public int RowIndex { get { return rowIndex; } set => rowIndex = value; }
+    public int ColumnIndex { get { return columnIndex; } set => columnIndex = value; }
+    public int RowSpam { get { return rowSpan; } set => rowSpan = value; }
+    public int ColumnSpan { get { return columnSpan; } set => columnSpan = value; }
+    private string name;
+    private string content;
+    private int rowIndex;
+    private int columnIndex;
+    private int rowSpan;
+    private int columnSpan;
+}
+
