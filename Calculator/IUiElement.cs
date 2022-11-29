9138d0dea82593331e2b905836fdecc08be390cc
Alim Lyukmanov <arlyukmanov@mail.ru>
2022-11-29 03:40:06 +0300

    Добавлен интерфейс для элеметов UI

---
+++ Calculator/IUiElement.cs
@@ -0,0 +1,9 @@
+﻿interface IUiElement
+{
+    string Name { get; set; }
+    string Content { get; set; }
+    int RowIndex { get; set; }
+    int ColumnIndex { get; set; }
+    int RowSpam { get; set; }
+    int ColumnSpan { get; set; }
+}
